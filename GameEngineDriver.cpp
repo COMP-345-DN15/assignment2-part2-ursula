ea3ddd72f862847d9a4a9d90fa434b7dd972244d
zhang bo <maybe35@163.com>
2021-11-16 09:41:14 -0500

    add part 3 main call.  assign Territory part and order method need to modify

--- GameEngineDriver.cpp
+++ GameEngineDriver.cpp
@@ -5,93 +5,68 @@
 // #include "Player.h"
 // #include "Cards.h"
 
-// Game engine class
-
- //constructor
-GameEngine::GameEngine(vector<Player*> t)
+int main()
 {
-    this->playerList = t;
+	//game Map1
+	vector <Territory*> Map1;
+	Territory a1("Canada", "NA", "Player1", 0);
+	Territory a2("USA", "NA", "Player1", 0);
+	Territory a3("Maxico", "NA", "Player1", 0);
+	Territory a4("Europe", "EU", "Player2", 0);
+	Territory a5("Africa", "AF", "Player2", 0);
+	Territory a6("SouthAmerica", "SA", "Player2", 0);
+	Territory a7("Russia", "AS", "Player2", 0);
+	Territory a8("China", "AS", "Player3", 0);
+	Territory a9("Japan", "AS", "Player3", 0);
+	Territory a10("Australia", "AU", "Player3", 0);
+	Map1.push_back(&a1);
+	Map1.push_back(&a2);
+	Map1.push_back(&a3);
+	Map1.push_back(&a4);
+	Map1.push_back(&a5);
+	Map1.push_back(&a6);
+	Map1.push_back(&a7);
+	Map1.push_back(&a8);
+	Map1.push_back(&a9);
+	Map1.push_back(&a10);
+
+	vector<Cards*> card1;
+	vector<Cards*> card2;
+	vector<Cards*> card3;
+
+	vector<Order*> Orderlist1;
+	vector<Order*> Orderlist2;
+	vector<Order*> Orderlist3;
+
+	vector<Player*> playerList;
+	playerList.push_back(new Player(10, "Player1", Map1, card1, Orderlist1));
+	playerList.push_back(new Player(20, "Player2", Map1, card1, Orderlist1));
+	playerList.push_back(new Player(40, "Player3", Map1, card1, Orderlist1));
+	GameEngine game(playerList);
+
+
+
+	bool check = true;
+
+	while (check)
+	{
+		game.reinforcmentPhase();
+		game.issueOrdersPhase();
+		game.excuteOrderPhase();
+
+		for (size_t i = 0; i < playerList.size(); i++)
+		{
+			if (playerList[i]->toDefend().size() == 10 || playerList.size() == 1)
+			{
+				cout << "Winner is " << playerList[i]->getName() << endl;
+				check = false;
+			}
+			if (playerList[i]->toDefend().size() == 0)
+			{
+				cout << playerList[i]->getName() << " lost the game." << endl;
+				playerList.erase(playerList.begin() + i);
+			}
+		}
+	}
+	return 0;
 }
-
-//destructor
-GameEngine::~GameEngine() {
-    for (auto p : playerList)
-    {
-        delete p;
-    }
-    playerList.clear();
-}
-
-bool LessSort(Order* a, Order* b)
-{
-    return(a->get_type_id() < b->get_type_id());
-}
-
-//Calculate player's reinforcement
-void GameEngine::reinforcmentPhase()
-{
-    bool check = false;
-    int temp = 0;
-    double count = 0;
-
-    for (size_t i = 0; i < (playerList.size()); i++)
-    {   //check the player's terriotries
-        temp = playerList[i]->getReinforcementPool();
-        vector<Territory*> c1 = playerList[i]->toDefend();
-
-
-        //count the terriorties number
-
-        count = c1.size();
-        int s1 = (int)round(count / 3);
-
-        //calcuate the contient bouns
-        check = playerList[i]->playerContientBouns();
-        cout << temp << endl;
-
-        if (check == true)
-        {
-            temp += 2 * s1;
-        }
-        else
-        {
-            temp = temp + s1;
-            cout << temp << endl;
-
-
-        }
-        temp = temp + 3;
-
-        cout << temp << endl;
-        playerList[i]->setReinforcementPool(temp);
-        check = false;
-        temp = 0;
-        count = 0;
-
-    }
-}
-void GameEngine::issueOrdersPhase()
-{
-    for (size_t i = 0; i < (playerList.size()); i++)
-    {
-        playerList[i]->issueOrder();
-
-    }
-
-}
-
-void GameEngine::excuteOrderPhase()
-{
-    vector<Order*> sortOrder;
-
-    for (size_t i = 0; i < (playerList.size()); i++)
-    {
-
-        sort(playerList[i]->getOrderList().begin(), playerList[i]->getOrderList().end(), LessSort);
-        for (int j = 0; i < playerList[i]->getOrderList().size(); j++)
-        {
-            playerList[i]->getOrderList()[j]->execute();
-        }
-    }
-}
-
