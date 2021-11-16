9d41137be71432cea3530effa23f11f2fbf16da9
zhang bo <maybe35@163.com>
2021-11-16 09:27:11 -0500

    update play.cpp add needed methods

--- Player.cpp
+++ Player.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <ostream>
 #include <typeinfo>
@@ -9,28 +8,68 @@ using namespace std;
 
 
 //Default constructor
-Player::Player(){}
+Player::Player()
+{
+	name = "default";
+	int reinforcementPool = 0;
+	vector<Territory*> territory;
+	vector<Cards*> handCard;
+	vector<Order*> listOfOrders;
+}
+
 
 //Parametric construtor 
 Player::Player(string pname)
 {
-    cout << "creating player " << pname << endl;
 	name = pname;
-//	playerHand = new Hand();
 }
 
+//Copy constructor (Deep copy)
+Player::Player(const Player& p)
+{
+	this->reinforcementPool = p.reinforcementPool;
+	this->name = p.name;
+	this->territory = p.territory;
+	this->handCard = p.handCard;
+	this->listOfOrders = p.listOfOrders;
+
+}
+
+//Four parmeter constructor
+Player::Player(int reinforcementPool, string name, vector<Territory*> t, vector<Cards*> h, vector<Order*> o)
+{
+	this->reinforcementPool = reinforcementPool;
+	this->name = name;
+	this->territory = t;
+	this->handCard = h;
+	this->listOfOrders = o;
+}
+
+//added assignment operator
+Player& Player::operator=(const Player& player)
+{
+	this->name = player.name;
+	this->reinforcementPool = player.reinforcementPool;
+	this->territory = player.territory;
+	this->handCard = player.handCard;
+	this->listOfOrders = player.listOfOrders;
+	return *this;
+}
 
 // Player destructor
 Player::~Player()
 {
-//	delete cards;
-//	cards = nullptr;
-	for (auto p : listOfTerritoriesOwned)
+	for (auto p : handCard)
 	{
-        // TODO: This causes a SIGABRT Error on my machine - deleting unassigned memory
-//		delete p;
+		delete p;
 	}
-	listOfTerritoriesOwned.clear();
+	handCard.clear();
+
+	for (auto p : territory)
+	{
+		delete p;
+	}
+	territory.clear();
 
 	for (auto p : listOfTerritoriesToDefend)
 	{
@@ -46,11 +85,77 @@ Player::~Player()
 
 	for (auto p : listOfOrders)
 	{
-        cout << "now deleting p: " << *p << endl;
 		delete p;
 	}
 	listOfOrders.clear();
+
+	for (auto p : handCard)
+	{
+		delete p;
+	}
+	handCard.clear();
 }
+
+string Player::getName() {
+	return this->name;
+}
+
+void Player::setName(string s) {
+	this->name = s;
+}
+
+int Player::getReinforcementPool() {
+	return this->reinforcementPool;
+}
+
+void Player::setReinforcementPool(int i) {
+	this->reinforcementPool = i;
+}
+
+void Player::setTerritory(Territory t)
+{
+
+	this->territory.push_back(&t);
+}
+
+vector<Territory*> Player::getTerritory()
+{
+	return territory;
+
+}
+
+vector<Territory*> Player::get_neighbour_territories(vector<Territory*> Map)
+{
+	vector<Territory*> neighbouring_terrritories;
+	for (size_t i = 0; i < territory.size(); i++)
+	{
+		for (size_t j = 0; j < Map.size(); j++)
+		{
+			if (!territory[i]->getTerritoryOwner().compare(Map[j]->getTerritoryOwner()))
+			{
+				for (int k = 0; k < neighbouring_terrritories.size(); k++)
+				{
+					if (!neighbouring_terrritories[k]->getTerritoryOwner().compare(Map[j]->getTerritoryOwner()) || neighbouring_terrritories.empty())
+					{
+						neighbouring_terrritories.push_back(Map[j]);
+					}
+				}
+
+			}
+		}
+
+	}
+
+	return neighbouring_terrritories;
+
+}
+
+
+vector<Cards*> Player::getCard()
+{
+	return handCard;
+}
+
 
 
 //ToAttack() method return a list of territories
@@ -78,18 +183,114 @@ vector<Territory*> Player::toDefend() {
 
 //IssueOrder() will creat a order obj and add it to player's order list
 void Player::issueOrder()
-{	//every time make a new order obj there will be a printout to show it works
-    vector<Order*> listOfOrders;
-	Order* ord = new Bomb();
-	listOfOrders.push_back(ord);
-	std::cout << *ord << std::endl;
+{
+	//Display territories that can be attack or defend
+	vector<Territory*> AttackList;
+	AttackList = toAttack();
+	vector<Territory*> DefendList;
+	DefendList = toDefend();
+
+	//Deploy order until no armies left	
+	while (getReinforcementPool() != 0)
+	{
+		int army = getReinforcementPool();
+
+		for (size_t i = 0; i < DefendList.size(); i++)
+		{
+
+			int temp = rand() % (army + 1);
+			cout << temp << endl;
+			temp += DefendList[i]->getArmyAmount();
+			DefendList[i]->setArmyAmount(temp);
+			army = temp - army;
+			setReinforcementPool(army);
+			if (army == 1)
+			{
+				temp = 1;
+				temp += DefendList[i]->getArmyAmount();
+				DefendList[i]->setArmyAmount(temp);
+				setReinforcementPool(0);
+			}
+		}
+	}
+	//Advance order
+	srand((unsigned int)time(NULL));
+	int actionNumber = rand() % AttackList.size();
+
+	int Enemy = AttackList[actionNumber]->getArmyAmount();
 }
+
+bool Player::playerContientBouns()
+{
+	string a = "NA";
+	string b = "AS";
+	string c = "SA";
+	string d = "AU";
+	string e = "EU";
+	string f = "AF";
+
+	int c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0, c6 = 0;
+	for (size_t i = 0; i < territory.size(); i++)
+	{
+		string temp = (*territory[i]).getContinent();
+		if (temp.compare(a) == 0)
+		{
+			c1++;
+		}
+		if (temp.compare(b) == 0)
+		{
+			c2++;
+		}
+		if (temp.compare(c) == 0)
+		{
+			c3++;
+		}
+		if (temp.compare(d) == 0)
+		{
+			c4++;
+		}
+		if (temp.compare(e) == 0)
+		{
+			c5++;
+		}
+		if (temp.compare(f) == 0)
+		{
+			c6++;
+		}
+
+
+	}
+	if (c1 == 3) { return true; }//NA 
+	if (c2 == 3) { return true; }//AS
+	if (c3 == 1) { return true; }//SA
+	if (c4 == 1) { return true; }//AU
+	if (c5 == 1) { return true; }//EU
+	if (c6 == 1) { return true; }//EU
+
+
+
+	return false;
+}
+
+vector<Order*> Player::getOrderList()
+{
+
+	return listOfOrders;
+}
+
+
+void Player::printOrder()
+{
+
+	for (size_t i = 0; i < listOfOrders.size(); i++) {
+		cout << "<" << listOfOrders.at(i)->getResult() << "> ";
+	}
+	cout << endl;
+}
+
 void  Player::print()
 {
 	cout << "this is player:"<<name<<endl;
 }
 
-//Army::Army(int armyNumber) {
-//    this->armyNumber = armyNumber;
-//}
 
