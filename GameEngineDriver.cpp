11990894d288a425abc85e7965088662a6c9c655
zhang bo <maybe35@163.com>
2021-11-16 09:36:12 -0500

    update GameEngine.cpp part 3 methods

--- GameEngineDriver.cpp
+++ GameEngineDriver.cpp
@@ -1,26 +1,97 @@
-#include <iostream>
 #include "GameEngine.h"
-#include "GameEngineDriver.h"
-#include "CommandProcessor.h"
+#include <iomanip>
+#include <algorithm>
+// Final names of other .h files TBD
+// #include "Player.h"
+// #include "Cards.h"
 
-using namespace std;
+// Game engine class
 
-// GameEngine Driver
-
-
-
-void testGameEngine()
+ //constructor
+GameEngine::GameEngine(vector<Player*> t)
 {
-	cout << "===================" << endl;
-	cout << "Welcome to Warzone" << endl;
-	cout << "===================\n" << endl;
-	cout << "Testing game engine\n" << endl;
-
-	GameEngine* gameEngine = new GameEngine();
-
-	gameEngine->startGame();
-
-	delete gameEngine;
+    this->playerList = t;
 }
 
+//destructor
+GameEngine::~GameEngine() {
+    for (auto p : playerList)
+    {
+        delete p;
+    }
+    playerList.clear();
+}
+
+bool LessSort(Order* a, Order* b)
+{
+    return(a->get_type_id() < b->get_type_id());
+}
+
+//Calculate player's reinforcement
+void GameEngine::reinforcmentPhase()
+{
+    bool check = false;
+    int temp = 0;
+    double count = 0;
+
+    for (size_t i = 0; i < (playerList.size()); i++)
+    {   //check the player's terriotries
+        temp = playerList[i]->getReinforcementPool();
+        vector<Territory*> c1 = playerList[i]->toDefend();
+
+
+        //count the terriorties number
+
+        count = c1.size();
+        int s1 = (int)round(count / 3);
+
+        //calcuate the contient bouns
+        check = playerList[i]->playerContientBouns();
+        cout << temp << endl;
+
+        if (check == true)
+        {
+            temp += 2 * s1;
+        }
+        else
+        {
+            temp = temp + s1;
+            cout << temp << endl;
+
+
+        }
+        temp = temp + 3;
+
+        cout << temp << endl;
+        playerList[i]->setReinforcementPool(temp);
+        check = false;
+        temp = 0;
+        count = 0;
+
+    }
+}
+void GameEngine::issueOrdersPhase()
+{
+    for (size_t i = 0; i < (playerList.size()); i++)
+    {
+        playerList[i]->issueOrder();
+
+    }
+
+}
+
+void GameEngine::excuteOrderPhase()
+{
+    vector<Order*> sortOrder;
+
+    for (size_t i = 0; i < (playerList.size()); i++)
+    {
+
+        sort(playerList[i]->getOrderList().begin(), playerList[i]->getOrderList().end(), LessSort);
+        for (int j = 0; i < playerList[i]->getOrderList().size(); j++)
+        {
+            playerList[i]->getOrderList()[j]->execute();
+        }
+    }
+}
 
