2b1f5b4dcf7b3d24a1a405d89fb03714315d285f
zhang bo <maybe35@163.com>
2021-11-16 09:26:11 -0500

    Update Player.h
    add methods needed

--- Player.h
+++ Player.h
@@ -4,13 +4,9 @@
 #include "Cards.h"
 #include "Orders.h"
 #include "Map.h"
-
+#pragma once
 using namespace std;
 
-//struct Army {
-//    int armyNumber;
-//    Army(int armyNumber);
-//};
 
 class Player {
 
@@ -18,35 +14,47 @@ public:
 	
 	string name;
 	// vectors that contain a list of Territory objects
-	vector<Territory*> listOfTerritoriesOwned;
+	vector<Territory*> territory;
 	vector<Territory*> listOfTerritoriesToAttack;
 	vector<Territory*> listOfTerritoriesToDefend;
-    // vector that contains a list of Order objects
-    vector<Order*> listOfOrders;
 
-	Player();
+	Player();// Default constructor
 	Player(string pname);
+	Player(const Player& p); //Copy constructor
+	Player(int, string, vector<Territory*> territories, vector<Cards*> handCard, vector<Order*> order); //Constructor
 	~Player();
+	Player& operator = (const Player&);
+
 	// methods that return corresponding list
 	vector<Territory*> toDefend();
 	vector<Territory*> toAttack();
+	vector<Order*> getOrderList();
+	vector<Cards*> getCard();
+	vector<Territory*> getTerritory();
+	vector<Territory*> get_neighbour_territories(vector<Territory*>);
 
 	// methods that create and add an order to list of orders.
 	void issueOrder();
+	string getName();
+	void setName(string);
+	int getReinforcementPool();
+	void setReinforcementPool(int);
+	void setTerritory(Territory);
+	void printOrder();
+	bool playerContientBouns();
 	void print();
-//    vector<Hand> cards;
-
-//    vector<Hand> cardsHandVector;
-    Hand playerHand;
-    
-//    vector<Army> reinforcementPool;
-    vector<int> reinforcementPool;
 
 private:
 
+	int reinforcementPool;
+	// vector that contains a list of Order objects
+	vector<Order*> listOfOrders;
+
 	// vector that contains a list of Card objects
-//	Hand* cards;
-    
+	vector<Cards*> handCard;
 
+	//friend classes
+	friend class Cards;
+	friend class GameEngine;
+	friend class Map;
 };
-
